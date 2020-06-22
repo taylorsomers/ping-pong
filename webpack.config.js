8e86c78fd1c1957210cd60b544d1d02a8846cc15
Taylor Somers <tsomers@yandex.com>
2020-06-22 14:11:18 -0400

    Add clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Ping Pong',
       template: './src/index.html',
