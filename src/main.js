35d9dd8a7545113a708592d8a814409e3dfff7cd
Taylor Somers <tsomers@yandex.com>
2020-06-22 13:28:22 -0400

    Add src directory as well as main js and ping-pong js files

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    const goal = $('#goal').val();
+    const output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
